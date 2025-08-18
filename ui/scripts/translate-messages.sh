23a8487ba06743af823069e02295bc0d0dd5d89d
Andrew Bengier <abengier@MacBookPro.localdomain>
2025-08-18 12:00:20 -0400

    Update translation script

--- ui/scripts/translate-messages.sh
+++ ui/scripts/translate-messages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check files for supported languages
+# Check files for default supported languages
 supported_languages=(
     "en" 
     # "es" 
@@ -12,10 +12,46 @@ translationsDiretory="$appDirectory/src/i18n/"
 
 fullMode=false
 
-if [[ "$@" =~ "--full" ]]; then
-  echo "Full mode enabled."
-  fullMode=true
-fi
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "$package - attempt to capture frames"
+      echo " "
+      echo "$package [options] application [arguments]"
+      echo " "
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "-a, --action=ACTION       specify an action to use"
+      echo "-o, --output-dir=DIR      specify a directory to store output in"
+      exit 0
+      ;;
+    -f|--full)
+      shift
+      echo "Full mode enabled."
+      fullMode=true
+      shift
+      ;;
+    -l|--lang)
+      shift
+      if test $# -gt 0; then
+        language=$1
+        if [[ ! "$language" = "${supported_languages[*]}" ]]; then
+          echo "'${language}' added"
+          supported_languages+=(${language})
+        else
+          echo Language ${language} already supported
+        fi
+      else
+        echo "no language specified"
+        exit 1
+      fi
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
 
 # [TEST]
 # echo "App Directory: $appDirectory"
